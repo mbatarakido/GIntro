5b82c56f6da453454563063721bd22b3e7fde765
mbatarakido <rakido@proton.me>
2026-08-26 23:03:56 +0300

    new BeanFactory object definition

--- Java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -18,7 +18,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
+		// new BeanFactory object definition
 		BeanFactory factory = new BeanFactory() {
 			@Override
 			public Object getBean(String name) throws BeansException {
