890dcaf4743a952afcdaa4c8b343636c5c38c27b
mbatarakido <rakido@proton.me>
2026-08-26 23:23:46 +0300

    ClassPathXmlApplicationContext

--- Java/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Java/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.ResolvableType;
 import org.springframework.core.io.ClassPathResource;
@@ -18,88 +19,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		// new BeanFactory object definition, maybe not the latest way of defining a BeanFactory class
-		BeanFactory factory = new BeanFactory() {
-			@Override
-			public Object getBean(String name) throws BeansException {
-				return null;
-			}
-
-			@Override
-			public <T> T getBean(String name, Class<T> requiredType) throws BeansException {
-				return null;
-			}
-
-			@Override
-			public Object getBean(String name, @Nullable Object @Nullable ... args) throws BeansException {
-				return null;
-			}
-
-			@Override
-			public <T> T getBean(Class<T> requiredType) throws BeansException {
-				return null;
-			}
-
-			@Override
-			public <T> T getBean(Class<T> requiredType, @Nullable Object @Nullable ... args) throws BeansException {
-				return null;
-			}
-
-			@Override
-			public <T> ObjectProvider<T> getBeanProvider(Class<T> requiredType) {
-				return null;
-			}
-
-			@Override
-			public <T> ObjectProvider<T> getBeanProvider(ResolvableType requiredType) {
-				return null;
-			}
-
-			@Override
-			public <T> ObjectProvider<T> getBeanProvider(ParameterizedTypeReference<T> requiredType) {
-				return null;
-			}
-
-			@Override
-			public boolean containsBean(String name) {
-				return false;
-			}
-
-			@Override
-			public boolean isSingleton(String name) throws NoSuchBeanDefinitionException {
-				return false;
-			}
-
-			@Override
-			public boolean isPrototype(String name) throws NoSuchBeanDefinitionException {
-				return false;
-			}
-
-			@Override
-			public boolean isTypeMatch(String name, ResolvableType typeToMatch) throws NoSuchBeanDefinitionException {
-				return false;
-			}
-
-			@Override
-			public boolean isTypeMatch(String name, Class<?> typeToMatch) throws NoSuchBeanDefinitionException {
-				return false;
-			}
-
-			@Override
-			public @Nullable Class<?> getType(String name) throws NoSuchBeanDefinitionException {
-				return null;
-			}
-
-			@Override
-			public @Nullable Class<?> getType(String name, boolean allowFactoryBeanInit) throws NoSuchBeanDefinitionException {
-				return null;
-			}
-
-			@Override
-			public String[] getAliases(String name) {
-				return new String[0];
-			}
-		};
+		BeanFactory factory = new ClassPathXmlApplicationContext();
 		MessageWriter messageWriter = (MessageWriter) factory.getBean("messageWriter", MessageWriter.class);
 		IMessageProducer messageProducer = (IMessageProducer) factory.getBean("htmlMessageProducer", IMessageProducer.class);
 
@@ -107,5 +27,4 @@ public class DemoApplication {
 		messageWriter.writeMessage("Welcome to Spring");
 		//SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
