92ca4a8d4f4a61358265a23d43dd690cd6eba696
mbatarakido <rakido@proton.me>
2026-08-29 21:14:09 +0300

    Python lists

--- Python/main.py
+++ Python/main.py
@@ -4,6 +4,11 @@ print(list)
 print(type(list))
 
 
-l=tuple(range(0,20,2))
+l = tuple(range(0,20,2))
+print(l)
+print(type(l))
+
+s = "Python Automation workflow journey"
+l=s.split()
 print(l)
 print(type(l))
